b09a4fdb444c87d708f82b6c5affba236807f13e
qiongshu <zhangqiongshu@gmail.com>
2019-12-20 17:07:29 +0900

    setting up collapsing toolbar for detail page

--- app/src/main/java/com/example/mymoviedemo/ui/DetailPageFragment.java
+++ app/src/main/java/com/example/mymoviedemo/ui/DetailPageFragment.java
@@ -1,5 +1,6 @@
 package com.example.mymoviedemo.ui;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -7,20 +8,45 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.mymoviedemo.DetailPageViewModel;
 import com.example.mymoviedemo.R;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DetailPageFragment extends Fragment {
-
+    private static final String TAG = "DetailPageFragment";
     private DetailPageViewModel mViewModel;
+    private AppBarLayout appBarLayout;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private boolean favoriteBtnEnabled;
+    private FloatingActionButton floatingActionButton;
+    private NavController navController;
+    private Toolbar toolbar;
 
     public static DetailPageFragment newInstance() {
         return new DetailPageFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setHasOptionsMenu(favoriteBtnEnabled);
     }
 
     @Override
@@ -30,10 +56,76 @@ public class DetailPageFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupCollapsingAppbar(view);
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(DetailPageViewModel.class);
         // TODO: Use the ViewModel
     }
 
+    private void setupCollapsingAppbar(View view) {
+        navController = Navigation.findNavController(view);
+        toolbar = view.findViewById(R.id.detail_toolbar);
+        toolbar.inflateMenu(R.menu.detail_page_menu);
+        final MenuItem favoriteBtn2 = toolbar.getMenu().findItem(R.id.favorite_btn_2);
+        favoriteBtn2.setVisible(false);
+        appBarLayout = view.findViewById(R.id.app_bar);
+        floatingActionButton = view.findViewById(R.id.favorite_btn);
+        collapsingToolbarLayout = view.findViewById(R.id.collapsing_toolbar_layout);
+        collapsingToolbarLayout.setTitle("dummy title");
+
+        NavigationUI.setupWithNavController(collapsingToolbarLayout,toolbar,navController);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                Log.i(TAG, "onOffsetChanged: veritical offset: "+verticalOffset);
+                if(Math.abs(verticalOffset) >=appBarLayout.getTotalScrollRange()-56){//appbar is fully collapsed
+                    favoriteBtn2.setVisible(true);
+                }else{
+                    favoriteBtn2.setVisible(false);
+                }
+                //setHasOptionsMenu(favoriteBtnEnabled);
+            }
+        });
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchFavorite();
+            }
+        });
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.favorite_btn_2:
+                        switchFavorite();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
+    }
+
+
+/*
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu,inflater);
+        inflater.inflate(R.menu.detail_page_menu,menu);
+    }*/
+
+
+
+    private void switchFavorite() {
+    }
 }
--- app/src/main/java/com/example/mymoviedemo/ui/MainActivity.java
+++ app/src/main/java/com/example/mymoviedemo/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mymoviedemo.ui;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
@@ -8,6 +9,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -17,6 +19,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.mymoviedemo.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -31,7 +34,7 @@ import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.support.HasSupportFragmentInjector;
 
-public class MainActivity extends AppCompatActivity implements HasSupportFragmentInjector,BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements HasSupportFragmentInjector, BottomNavigationView.OnNavigationItemSelectedListener, NavController.OnDestinationChangedListener {
     private static final String TAG = "MainActivity";
     @Inject
     public DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
@@ -62,14 +65,13 @@ public class MainActivity extends AppCompatActivity implements HasSupportFragmen
     private void setupNavigation() {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         appBarConfiguration = new AppBarConfiguration.Builder(R.id.mainPageFragment,R.id.favoriteMoviesFragment).build();
         navController = Navigation.findNavController(this,R.id.nav_host_fragment);
         bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         NavigationUI.setupWithNavController(bottomNavigationView,navController);
         NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
-
+        navController.addOnDestinationChangedListener(this);
     }
 
     @Override
@@ -111,5 +113,16 @@ public class MainActivity extends AppCompatActivity implements HasSupportFragmen
                 break;
         }
         return false;
+    }
+
+    @Override
+    public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+        if(destination.getId()==R.id.detailPageFragment){
+            bottomNavigationView.setVisibility(View.GONE);
+            toolbar.setVisibility(View.GONE);
+        }else{
+            bottomNavigationView.setVisibility(View.VISIBLE);
+            toolbar.setVisibility(View.VISIBLE);
+        }
     }
 }
